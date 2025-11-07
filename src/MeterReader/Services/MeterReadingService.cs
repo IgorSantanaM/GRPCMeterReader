9f990195fc8371bb1800207b2de667dc7ec8ba28
Igor Santana Medeiros <1434@adm.ucdb.br>
2025-11-07 14:12:32 -0400

    (feat): Added gRPC protobuffs

---
+++ src/MeterReader/Services/MeterReadingService.cs
@@ -0,0 +1,35 @@
+﻿using Grpc.Core;
+using MeterReader.gRPC;
+using static MeterReader.gRPC.MeterReaderService;
+
+namespace MeterReader.Services
+{
+    public class MeterReadingService(IReadingRepository repository, ILogger<MeterReadingService> logger) : MeterReaderServiceBase
+    {
+        public override async Task<StatusMessage> AddReading(ReadingPacket request, ServerCallContext context)
+        {
+            if (request.Succesful == ReadingStatus.Success)
+            {
+                foreach (var reading in request.Readings)
+                {
+                    var readingValue = new MeterReading()
+                    {
+                        CustomerId = reading.CustomerId,
+                        Value = reading.ReadingValue,
+                        ReadingDate = reading.ReadingTime.ToDateTime()
+                    };
+
+                    repository.AddEntity(readingValue);
+                }
+                if (await repository.SaveAllAsync())
+                {
+                    return new StatusMessage
+                    {
+                        Message = "Readings successfully saved.",
+                        Success = ReadingStatus.Success
+                    };
+                }
+            }
+        }
+    }
+}
